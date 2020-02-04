3008f8cacec3d027a9595c7a3ffa7a7b85daba16
lchaconc <luis.chacon.campos@gmail.com>
2020-02-04 14:31:38 -0600

    fin render de recursos
    Pendiente probar las demás categorías

--- src/componentes/VerRecursos.js
+++ src/componentes/VerRecursos.js
@@ -1,12 +1,27 @@
 import React, { useState } from 'react';
 import obtener from '../modulos/obtener';
+var dataset=null;
 
 
-obtener("https://pokeapi.co/api/v2/gender/1/", function (data) {
-    console.log("data", data);
+obtener("http://localhost/faro/webservices/obtener_recursos.php", function (data) {    
+    dataset = data;
+    console.log("data", dataset);
 })
 
-function VerRecursos() {
+function VerRecursos() {    
+    const [dataFiltrados, setDataFiltrados] = useState(null);
+
+    const handleFiltrarRecursos =(e)=>{
+        const categoria = e.target.value;
+        const limite = dataset.length;
+        var tmpDataset = [];
+        for (let index = 0; index < limite; index++) {            
+            if (categoria === dataset[index].nivel  ) {
+                tmpDataset.push(dataset[index]);
+            }
+        }      
+        setDataFiltrados(tmpDataset);
+    }
 
     return (
         <React.Fragment>
@@ -18,12 +33,14 @@ function VerRecursos() {
                 <div className="input-group-prepend">
                     <label className="input-group-text" htmlFor="selCategoria">Categoría</label>
                 </div>
-                <select className="custom-select" id="selCategoria">
+               <div className="col-6">
+               <select onChange={handleFiltrarRecursos} className="custom-select" id="selCategoria">                    
                     <option defaultValue >Seleccione un opción</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
+                    <option value="prescolar">Preescolar</option>
+                    <option value="primaria"> Primaria </option>
+                    <option value="secundaria">Secundaria</option>                    
                 </select>
+               </div>
             </div>
 
             <table className="table table-striped">
@@ -31,29 +48,33 @@ function VerRecursos() {
                     <tr>
                         <th scope="col">#</th>
                         <th scope="col">Nombre</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
+                        <th scope="col">Descripción</th>
+                        <th scope="col">Año</th>
+                        <th scope="col"> Editar </th>
+                        <th scope="col"> Eliminar </th>
+                        
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
+                      {
+                          dataFiltrados !== null && 
+                          (
+                            dataFiltrados.map((item, i)=>(
+                            <tr key={"recurso"+i}>
+                                <th scope="row">{i+1}</th>
+                                <td>{item.nombre}</td>
+                                <td>{item.descripcion}</td>
+                                <td>{item.anno}</td>
+                                <td>
+                                    <i className="fas fa-pencil-alt"></i>
+                                </td>
+                                <td>
+                                    <i className="far fa-trash-alt"></i>
+                                </td>
+                            </tr> 
+                            ))
+                          )                     
+                      }                
                 </tbody>
             </table>
 

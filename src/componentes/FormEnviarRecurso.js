3afc2756a6d1638fb61547bd0f806a2899e6ff49
lchaconc <luis.chacon.campos@gmail.com>
2020-02-05 13:22:31 -0600

    inicio metodo select Nivel
    Se crea archivo Grupocheck para iniciar  el desarrollo de los chek de años por niveles

--- src/componentes/FormEnviarRecurso.js
+++ src/componentes/FormEnviarRecurso.js
@@ -1,26 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+const niveles = ["Prescolar", "Primaria", "Secundaria" ];
+
+
+
+
+
+
 
 function FormEnviarRecurso() {
+  const [nivel, setNivel] = useState(-1);
 
+  const handleSelNivel = (e) => {
+    console.log(e.target.value);
+    setNivel(e.target.nive)
+    
+  }
 
   return (
     <React.Fragment>
       <div className="alert alert-primary" role="alert">
-                Admin/Agregar recursos
+        Admin/Agregar recursos
       </div>
       <form>
         <div className="form-group">
-          <label htmlFor="txtNombre">Nombre del recurso:</label>
-          <input type="text" className="form-control" id="txtNombre" name="nombreRecurso" placeholder="Nombre" />
+          {//<label htmlFor="txtNombre">Nombre del recurso:</label>
+          }
+          <input type="text" className="form-control" id="txtNombre" name="nombre" placeholder="Nombre" />
         </div>
 
         <div className="form-group">
-          <label htmlFor="txtDescripcion">Descripción del recurso:</label>
-          <input type="text" className="form-control" id="txtDescripcion" name="descripcionRecurso" placeholder="Descripción" />
+        {
+          //<label htmlFor="txtDescripcion">Descripción del recurso:</label>
+        }
+          <input type="text" className="form-control" id="txtDescripcion" name="descripcion" placeholder="Descripción" />
         </div>
 
+        <div className="input-group mb-3">
+          <div className="input-group-prepend">
+            {//<label className="input-group-text" htmlFor="selnivel">Nivel</label>
+            }
+          </div>
+          <select onClick={handleSelNivel} className="custom-select" id="selnivel" name="id_nivel" >
+            <option defaultValue value={-1} >Seleccione un nivel</option>
+            {
+              niveles.map((item, i)=>(
+                <option key={"categoria"+i} value={i}> {item} </option>
+              ))
+            }
+          </select>
+        </div>
+
+     
+            {
+              //Grupo select
+            }
+
         <div className="form-group">
-          <label htmlFor="txtUrl">Url de educatico:</label>
+          {//<label htmlFor="txtUrl">Url de educatico:</label>
+          }
           <input type="text" className="form-control" id="txtUrl" name="url" placeholder="dirección de Educatico" />
         </div>
 
---
+++ src/componentes/GurpoCheck.js
@@ -0,0 +1,54 @@
+const annosPrimaria = [
+    {
+      "id" : 1,
+      "nombre" : "Primero"    
+    },
+    {
+      "id" : 2,
+      "nombre" : "Segundo"    
+    },
+    {
+      "id" : 3,
+      "nombre" : "Tercero"    
+    },
+    {
+      "id" : 4,
+      "nombre" : "Cuarto"    
+    },
+    {
+      "id" : 5,
+      "nombre" : "Quinto"    
+    },
+    {
+      "id" : 6,
+      "nombre" : "Sexto"    
+    }
+
+];
+const annosSecundaria = [
+{
+"id" : 7,
+"nombre" : "Sétimo"    
+},
+{
+"id" : 8,
+"nombre" : "Ocatavo"    
+},
+{
+"id" : 9,
+"nombre" : "Noveno"    
+},
+{
+"id" : 10,
+"nombre" : "Décimo"    
+},
+{
+"id" : 11,
+"nombre" : "Undécimo"    
+},
+{
+"id" : 12,
+"nombre" : "Duodécimo"    
+}
+
+];

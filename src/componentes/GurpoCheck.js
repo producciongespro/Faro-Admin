987296e7e509d6f20df036748b72a1bce146bc47
lchaconc <luis.chacon.campos@gmail.com>
2020-02-06 08:29:37 -0600

    fin imp checkbox primaria y secudnaria

--- src/componentes/GurpoCheck.js
+++ src/componentes/GurpoCheck.js
@@ -1,101 +1,95 @@
 import React from 'react';
 
 const annosPrimaria = [
-    {
-      "id" : 1,
-      "nombre" : "Primero"    
-    },
-    {
-      "id" : 2,
-      "nombre" : "Segundo"    
-    },
-    {
-      "id" : 3,
-      "nombre" : "Tercero"    
-    },
-    {
-      "id" : 4,
-      "nombre" : "Cuarto"    
-    },
-    {
-      "id" : 5,
-      "nombre" : "Quinto"    
-    },
-    {
-      "id" : 6,
-      "nombre" : "Sexto"    
-    }
+  {
+    "id": 1,
+    "nombre": "Primero"
+  },
+  {
+    "id": 2,
+    "nombre": "Segundo"
+  },
+  {
+    "id": 3,
+    "nombre": "Tercero"
+  },
+  {
+    "id": 4,
+    "nombre": "Cuarto"
+  },
+  {
+    "id": 5,
+    "nombre": "Quinto"
+  },
+  {
+    "id": 6,
+    "nombre": "Sexto"
+  }
 
 ];
 const annosSecundaria = [
-{
-"id" : 7,
-"nombre" : "Sétimo"    
-},
-{
-"id" : 8,
-"nombre" : "Ocatavo"    
-},
-{
-"id" : 9,
-"nombre" : "Noveno"    
-},
-{
-"id" : 10,
-"nombre" : "Décimo"    
-},
-{
-"id" : 11,
-"nombre" : "Undécimo"    
-},
-{
-"id" : 12,
-"nombre" : "Duodécimo"    
-}
+  {
+    "id": 7,
+    "nombre": "Sétimo"
+  },
+  {
+    "id": 8,
+    "nombre": "Ocatavo"
+  },
+  {
+    "id": 9,
+    "nombre": "Noveno"
+  },
+  {
+    "id": 10,
+    "nombre": "Décimo"
+  },
+  {
+    "id": 11,
+    "nombre": "Undécimo"
+  },
+  {
+    "id": 12,
+    "nombre": "Duodécimo"
+  }
 
 ];
 
 function GrupoCheck(props) {
-  
-  return(
-    
-      <React.Fragment>
-        {
-          (props.nivel === "1" ) &&
-          (
-        <div className="row">
-          <div className="col-12">
-          <div className="pretty p-default p-round p-thick">
-            <input type="checkbox" />
-            <div className="state p-primary-o">
-                <label>Primero</label>
-            </div>
-          </div>
 
-          <div className="pretty p-default p-round p-thick">
-            <input type="checkbox" />
-            <div className="state p-primary-o">
-                <label>Segundo</label>
-            </div>
-          </div>
+  return (
 
-          </div>
-        </div>
-          )
-        }
-             {
-          (props.nivel === "2") &&
-          (
-            <div className="pretty p-default p-round p-thick">
-            <input type="checkbox" />
-            <div className="state p-primary-o">
-                <label>Sétimo</label>
+    <div className="row my-2">
+      <div className="col-12">    
+      {
+        (props.nivel === "1") &&
+        (
+          annosPrimaria.map((item, i) => (
+            <div key={"primaria"+i} className="pretty p-default">
+              <input type="checkbox" value={item.id} />
+              <div className="state p-primary">
+                <label>{item.nombre}</label>
+              </div>
             </div>
-          </div>
-          )
-        }
-      </React.Fragment>
-    
+          ))
+        )
+      }
+      {
+        (props.nivel === "2") &&
+        (
+          annosSecundaria.map((item, i) => (
+            <div key={"primaria"+i} className="pretty p-default">
+              <input type="checkbox" value={item.id} />
+              <div className="state p-primary">
+                <label>{item.nombre}</label>
+              </div>
+            </div>
+          ))
+        )
+      }
+      </div>
+    </div>
+
   )
 }
 

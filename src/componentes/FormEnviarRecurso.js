6ada2ce78d2baecc0c68d2a906137131d170fdd3
lchaconc <luis.chacon.campos@gmail.com>
2020-02-10 15:11:25 -0600

    fase 1 imp y validacion hook
    pendiente hacer el Ajax

--- src/componentes/FormEnviarRecurso.js
+++ src/componentes/FormEnviarRecurso.js
@@ -1,48 +1,46 @@
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import GrupoCheck from './GurpoCheck';
 import obtenerValoresCheck from '../modulos/obtenerValoresCheck';
-import enviar from '../modulos/enviar';
+//import enviar from '../modulos/enviar';
 
 const niveles = ["Prescolar", "Primaria", "Secundaria", "Educación intercultural", "Educación Jóvenes y adultos", "Programa Nacional de Ferias", "Programa Bandera Azul"];
 const asignaturaPrimaria = ["Matemática", "Ciencias", "Español", "Estudios sociales", "Artes plásticas"];
-const asignaturaSecundaria = ["Matemática", "Ciencias", "Bioogía", "Química", "Español", "Estudios sociales", "Artes plásticas"];
+const asignaturaSecundaria = ["Matemática", "Ciencias", "Biología", "Química", "Español", "Estudios sociales", "Artes plásticas"];
 
-
-function FormEnviarRecurso() {
+export default function FormEnviarRecurso() {
   const [nivel, setNivel] = useState(-1);
+  const { register, handleSubmit, errors, getValues } = useForm();
 
-  const handleSelNivel = (e) => {
-    const nivel = parseInt(e.target.value);
-    console.log(e.target.value);
-    setNivel(nivel);
+
+  const onSubmit = data => {
+    console.log( "Antes del append", data);
+    const valorescheck = obtenerValoresCheck("anno");
+
+    data.anno = valorescheck;
+
+    console.log("DATA 2", data);
+    
+  }
+  console.log(errors);
+
+
+  const obtenerNivel = () => {
+    setNivel(parseInt(getValues().id_nivel));
   }
 
-  const handleEnviar = (e) => {
-    e.preventDefault();
-
-      
-
-    const data = {
-      "anno": obtenerValoresCheck("anno"),
-      "nombre": document.getElementById("txtNombre").value,      
-      "id_nivel": document.getElementById("selNivel").value,
-      "materia": document.getElementById("selMateria").value,
-      "descripcion": document.getElementById("txtDesc").value,
-      "url": document.getElementById("txtUrl").value,
-      "apoyo": document.getElementById("chkApoyo").checked,
-      "usuario": "pepito.campos.campos@mep.go.cr"
-
-    }
-    enviar("http://localhost/faro/webservices/test1.php", data);
-  }
 
 
   return (
+
     <React.Fragment>
       <div className="alert alert-primary" role="alert">
         Admin/Agregar recursos
       </div>
-      <form onSubmit={handleEnviar} id="form1" >
+
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+
 
         {
           //NIVEL: 
@@ -51,20 +49,21 @@ function FormEnviarRecurso() {
           <div className="input-group-prepend">
             <label className="input-group-text" htmlFor="selNivel">Nivel</label>
           </div>
-          <select onClick={handleSelNivel} className="custom-select" id="selNivel">
+          <select className="custom-select" name="id_nivel" ref={register} onChange={obtenerNivel}>
             <option defaultValue value={-1} >Seleccione un nivel</option>
             {
               niveles.map((item, i) => (
-                <option key={"categoria" + i} value={i}> {item} </option>
+                <option key={"nivel" + i} value={i}> {item} </option>
               ))
             }
           </select>
         </div>
 
         {
-          //CHECK AÑOS POR NIVEL: 
+          //Año por nivel          
         }
         <GrupoCheck nivel={nivel} nombre="anno" />
+
 
 
         {
@@ -75,7 +74,7 @@ function FormEnviarRecurso() {
               <div className="input-group-prepend">
                 <label className="input-group-text" htmlFor="selMateria">Asignatura</label>
               </div>
-              <select className="custom-select" id="selMateria">
+              <select className="custom-select" name="materia" id="selMateria" ref={register({ required: true })} >
                 <option defaultValue value={-1} >Seleccione la asignatura</option>
                 {
                   //Caso de primaria
@@ -100,8 +99,6 @@ function FormEnviarRecurso() {
           )
         }
 
-
-
         {
           //NOMBRE: 
         }
@@ -109,10 +106,8 @@ function FormEnviarRecurso() {
           <div className="input-group-prepend">
             <span className="input-group-text">Nombre</span>
           </div>
-          <input type="text" className="form-control" id="txtNombre" aria-label="Default" placeholder="Escriba aquí el nombre del recurso." />
+          <input type="text" className="form-control" name="nombre" id="txtNombre" aria-label="Default" placeholder="Escriba aquí el nombre del recurso." ref={register({ required: true, max: 32, min: 0, maxLength: 80 })} />
         </div>
-
-
 
         {
           //DESCRIPCIÓN: 
@@ -121,29 +116,57 @@ function FormEnviarRecurso() {
           <div className="input-group-prepend">
             <span className="input-group-text">Descripción</span>
           </div>
-          <textarea id="txtDesc" className="form-control" aria-label="With textarea" placeholder="Breve descripición del recurso" ></textarea>
+          <textarea 
+            id="txtDesc"
+            name="descripcion" 
+            className="form-control" 
+            aria-label="With textarea" 
+            placeholder="Breve descripición del recurso" 
+            ref={register}  
+          />
+            
         </div>
         <br />
 
 
+        {
+          //URL
+        }
         <div className="input-group mb-3">
           <div className="input-group-prepend">
             <span className="input-group-text" >URL</span>
           </div>
-          <input type="text" id="txtUrl" className="form-control" aria-label="Default" placeholder="Escriba la dirección web del recurso en Educatico." />
+          <input
+            type="text"
+            name="url"
+            id="txtUrl"
+            className="form-control"
+            aria-label="Default"
+            placeholder="Escriba la dirección web del recurso en Educatico."
+            ref={register({ required: true })}
+          />
         </div>
 
+        {
+          //Apoyo educativo
+        }
         <div className="pretty p-switch p-fill">
-          <input type="checkbox" id="chkApoyo" />
+          <input type="checkbox" 
+            id="chkApoyo"
+            name="apoyo" 
+            ref={register}
+            />
           <div className="state">
             <label>Apoyo educativo</label>
           </div>
         </div>
         <hr />
         <button type="submit" className="btn btn-primary"  > Enviar  </button>
+
+        
       </form>
+
     </React.Fragment>
-  )
+  );
 }
 
-export default FormEnviarRecurso;

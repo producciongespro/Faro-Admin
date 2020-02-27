1e7f34bdc90b1355966c5e0ea445dc80ed5c7b3b
lchaconc <luis.chacon.campos@gmail.com>
2020-02-27 15:03:17 -0600

    Fase 2: CAmbio info de modal

--- src/componentes/Modal.js
+++ src/componentes/Modal.js
@@ -1,35 +1,42 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Button, Modal } from 'react-bootstrap';
 
 
-function Modalc() {
-    const [show, setShow] = useState(false);
-  
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-  
-    return (
+function Modalc(props) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
       <>
-        <Button variant="primary" onClick={handleShow}>
-          Abrir Modal
+      <Button variant="primary" onClick={handleShow}>
+        Abrir Modal
         </Button>
-  
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edición</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="basic-addon1">Nombre del recurso</span>
+            </div>
+            <input type="text" className="form-control" placeholder="Nombre" aria-label="Username" aria-describedby="basic-addon1" />
+            </div>
+          </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
-              Close
+              Cerrar
             </Button>
             <Button variant="primary" onClick={handleClose}>
-              Save Changes
+              Guardar Cambios
             </Button>
           </Modal.Footer>
         </Modal>
       </>
-    );
-  }
-  
+      );
+    }
+    
   export default Modalc;

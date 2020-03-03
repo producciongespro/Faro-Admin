c7011f702c4d0467ed421ce164eeffdd8f5bf7fa
lchaconc <luis.chacon.campos@gmail.com>
2020-03-03 09:11:21 -0600

    imp Papelera reciclaje Fase 3
    -Renderizado de los campos.
    (Se utiliza  el componente tabla junto con el prop. modo para renderizar campos en específico)

--- src/componentes/Tabla.js
+++ src/componentes/Tabla.js
@@ -5,12 +5,27 @@ function Tabla(props) {
         <table id="tblNivel" className={props.clase}>
             <thead>
                 <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Nombre</th>
-                    <th scope="col">Año</th>
-                                 
-                                              
-                            <th scope="col"> Recuperar </th>                        
+                    <th className="text-center" scope="col">#</th>
+                    <th className="text-center" scope="col">Nombre</th>
+                    {
+                        props.modo === "papelera" &&                    
+                        <th className="text-center" scope="col"> Asignatura </th>                        
+                    }
+                    <th className="text-center" scope="col">Año</th>                                                 
+                    {
+                        props.modo === "visor" &&                    
+                        <th  className="text-center" scope="col"> Editar </th>
+                    }
+                    {
+                        props.modo === "visor" &&                    
+                        <th className="text-center" scope="col"> Eliminar </th>
+                    }
+                    {
+                        props.modo === "papelera" &&                    
+                        <th className="text-center" scope="col"> Recuperar </th>                        
+                    }
+                    
+                            
                 
 
                 </tr>
@@ -22,15 +37,35 @@ function Tabla(props) {
                         props.array.map((item, i) => (
                             <tr key={"recurso" + i}>
                                 <th scope="row">{i + 1}</th>
-                                <td>{item.nombre}</td>
-                                <td>{item.anno}</td>
-                              
-                                 
-                          
-                                                                
-                                        <td>
-                                            <i id={item.id} onClick={props.handleRecuperarRecurso} className="fas fa-trash-restore"></i>
-                                        </td>                                    
+                                <td >{item.nombre}</td>
+                                {
+                                    props.modo === "papelera" &&   
+                                    <td className="text-center">
+                                            {item.materia}
+                                    </td>                                    
+                                }
+                                <td className="text-center" >{item.anno}</td>
+ 
+                                {
+                                    props.modo === "papelera" &&   
+                                    <td className="text-center">
+                                            <i id={item.id} onClick={props.handleRecuperarRecurso} className="fas fa-recycle"></i>
+                                    </td>                                    
+                                } 
+                                {
+                                    props.modo === "visor" &&   
+                                     <td>
+                                     <i id={item.id} onClick={props.handleShow} className="fas fa-pencil-alt"></i>
+                                    </td>
+
+                                }
+                                {
+                                    props.modo === "visor" &&   
+                                    <td className="text-center" >
+                                        <i id={item.id} onClick={props.handleEliminarRecurso} className="far fa-trash-alt"></i>
+                                    </td>
+                                }                                                                  
+                                        
                                 
 
                             </tr>

0406074f7b7b84892e4699cbd37eb2af5831ead6
lchaconc <luis.chacon.campos@gmail.com>
2020-03-11 08:21:47 -0600

    Se elimkina comentarios en useefect

--- src/componentes/VerRecursos.js
+++ src/componentes/VerRecursos.js
@@ -59,9 +59,7 @@ function VerRecursos() {
     }, []);
 
     useEffect(() => {
-        console.log("Datos filtrados:", datosFiltrados);         
-        //console.log("En Espera", esperando);
-
+        console.log("Datos filtrados:", datosFiltrados);                 
     })
 
     const onSubmit = data => {

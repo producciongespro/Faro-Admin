c0e7979749545d4d5e0a6e18042bd4509ee91357
Patricia Hdz <32460268+patyhc4oct@users.noreply.github.com>
2021-04-20 08:39:10 -0600

    varios

---
+++ index/precache-manifest.d7ffc9742d4548f93aa9d23b713c9353.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c9977aeffabc86032cce815d7b0e75",
+    "url": "./index.html"
+  },
+  {
+    "revision": "080b0708ee3df051e610",
+    "url": "./static/css/2.e6b9452d.chunk.css"
+  },
+  {
+    "revision": "482042fb4692cb45e940",
+    "url": "./static/css/main.b594a094.chunk.css"
+  },
+  {
+    "revision": "080b0708ee3df051e610",
+    "url": "./static/js/2.a8f33e35.chunk.js"
+  },
+  {
+    "revision": "eba08dcb983c249067ba1a522c73e1ee",
+    "url": "./static/js/2.a8f33e35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "482042fb4692cb45e940",
+    "url": "./static/js/main.113aa910.chunk.js"
+  },
+  {
+    "revision": "49b8c823de766134fc77",
+    "url": "./static/js/runtime-main.8e1008dc.js"
+  },
+  {
+    "revision": "8b5cafe3646b39886fc3b53630ce2a0a",
+    "url": "./static/media/loading1.8b5cafe3.gif"
+  }
+]);

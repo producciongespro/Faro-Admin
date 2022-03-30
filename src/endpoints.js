bde5d041f9e45ab7fe16de862cb6dfdbab6c34da
arkoscr <32460304+arkoscr@users.noreply.github.com>
2022-03-30 13:27:59 -0600

    Cambio en archivo php a rutear para obtener bitacora (endpoints)

--- src/endpoints.js
+++ src/endpoints.js
@@ -10,7 +10,7 @@ const endpoints = {
     getSubODP: `${process.env.REACT_APP_URL_API}obtener_sub_categorias_odp.php`,
     getPoblacionesIDP: `${process.env.REACT_APP_URL_API}obtener_poblaciones_idp.php`,   
     getBotonesMenu: `${process.env.REACT_APP_URL_API}obtener_menu_botones.php`,
-    getBitacora: `${process.env.REACT_APP_URL_API}obtener_bitacora2.php?tabla=`,
+    getBitacora: `${process.env.REACT_APP_URL_API}obtener_bitacora.php?tabla=`,
     getODPBorrados: `${process.env.REACT_APP_URL_API}obtener_odp_borrado.php`,
 
     getODPBorrados: `${process.env.REACT_APP_URL_API}obtener_odp_borrado.php`,

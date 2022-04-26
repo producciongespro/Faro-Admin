70ba45fe2f7ab51757ece9cca561093d2c8fc7f7
lchaconc <luis.chacon.campos@gmail.com>
2022-04-26 09:06:07 -0600

    Envio query paramas en consultar bitacora -Instalación de librería gespro utils -Creación de método setup para llamarlo en useefect -Cambio en carga de datos mediante getData -Creación de método establecerTabla (Recibe el rol del usuario y devuelve núm de la tabla)

--- src/componentes/Bitacora.js
+++ src/componentes/Bitacora.js
@@ -1,52 +1,65 @@
-import React, {useState, useEffect, useContext} from 'react';
-import endpoints from '../endpoints';
-import Tabla from '../componentes/Tabla';
-import MyContext from '../modulos/MyContext';
+import React, { useState, useEffect, useContext } from "react";
+import endpoints from "../endpoints";
+import Tabla from "../componentes/Tabla";
+import MyContext from "../modulos/MyContext";
+import { getData } from "gespro-utils";
 
+function Bitacora(props) {
+  const [datosJson, setDataJson] = useState(null);
+  const { user } = useContext(MyContext);
+  console.log("USUARIO EN BITACORA >>>>>> ", user);
 
+  useEffect(() => {
+    setup();
+  }, []);
 
-function Bitacora (props) {
-    const [datosJson, setDataJson ] = useState(null);
-    const { user} = useContext(MyContext); 
-    console.log("USUARIO EN BITACORA >>>>>> ",user);       
+  useEffect(() => {
+    console.log("datosJson", datosJson);
+  });
 
-    useEffect(()=>{
+  const setup = async () => {
+    const URI = `${endpoints.getBitacora}${establecerTabla(user.role) }`;
+    console.log("URI", URI);
      
-    let urlAPI=  endpoints.getBitacora+user.role;        
-    console.log("urlAPI", urlAPI);
-        obtener(urlAPI);
-    },[]);
+    const res = await getData(   URI );
+    setDataJson(res.reverse());
+  };
 
-    useEffect(()=>{
-        console.log("datosJson",datosJson);        
-    })
+  const establecerTabla = (role) => {
+    let tabla = null;
+    switch (role) {
+      case "10":
+        //Recursos
+        tabla = 1;
+        break;
+      case "11":
+        //IDP
+        tabla = 2;
+        break;
+      case "12":
+        //Planeamientos
+        tabla = 3;
+        break;
 
-    async function obtener (urlAPI) {
-        let response = await fetch(urlAPI);
-        let data=  await response.json()                
-        setDataJson (  data.reverse() );               
-    }
+      default:
+          tabla=0
+        break;
+    };
+    return tabla;
+  };
 
-    return (
-        <React.Fragment>
-                <div className="alert alert-primary" role="alert">
-                    Admin/Bitácora
-                </div>
-                {
-                    datosJson !==null ?
-                    (                    
-                        <Tabla array={datosJson} clase="table table-striped" modo="bitacora" />
-                    ) :
-                    (
-                        <span>Por favor espere...</span>
-                    )
-                }
-
-                
-        </React.Fragment>
-    )
-    
+  return (
+    <React.Fragment>
+      <div className="alert alert-primary" role="alert">
+        Admin/Bitácora
+      </div>
+      {datosJson !== null ? (
+        <Tabla array={datosJson} clase="table table-striped" modo="bitacora" />
+      ) : (
+        <span>Por favor espere...</span>
+      )}
+    </React.Fragment>
+  );
 }
-
 
 export default Bitacora;
